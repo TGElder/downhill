fef3c4d49d914a2174d24bc7bd40401e1c11566c
TGElder <tgelder@gmail.com>
2017-09-06 19:54:49 +0100

    Adding getters, setters to Mesh plus utility to get 3x3 mesh

--- src/main/java/com/tgelder/downhill/Mesh.java
+++ src/main/java/com/tgelder/downhill/Mesh.java
@@ -16,10 +16,54 @@ public class Mesh {
 		y = new int[width][width];
 		z = new int[width][width];
 	}
-
-	boolean inBounds(int tx, int ty) {
-		return (tx>=0)&&(ty>=0)&&(tx<width)&&(ty<width);
+	
+	public final int getX(int mx, int my) {
+		return x[mx][my];
 	}
+	
+	public final int getY(int mx, int my) {
+		return y[mx][my];
+	}
+	
+	public final int getZ(int mx, int my) {
+		return z[mx][my];
+	}
+	
+	public final void setX(int mx, int my, int value) {
+		x[mx][my] = value;
+	}
+	
+	public final void setY(int mx, int my, int value) {
+		y[mx][my] = value;
+	}
+	
+	public final void setZ(int mx, int my, int value) {
+		z[mx][my] = value;
+	}
+	
+	public final void set(int mx, int my, int x, int y, int z) {
+		setX(mx,my,x);
+		setY(mx,my,y);
+		setZ(mx,my,z);
+	}
+	
+	public static Mesh of3x3(){
+		Mesh out = new Mesh(3);
+		
+		int max = Integer.MAX_VALUE;
+		int half = max/2;
+		
+		for (int x=0; x<3; x++) {
+			for (int y=0; y<3; y++) {
+				out.set(x, y, half*x, half*y, 0);
+			}
+		}
+		
+		out.setZ(0, 0, max);
+		
+		return out;
+	}
+
 	
 	
 }
